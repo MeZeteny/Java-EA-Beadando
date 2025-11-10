4a5f1dc32abf5b33cec66e7d85e5a82fc094763f
MeZeteny <198044126+MeZeteny@users.noreply.github.com>
2025-11-10 20:22:25 +0100

    Reszponzív téma

---
+++ src/main/java/hu/nje/beadando/controller/HomeController.java
@@ -0,0 +1,13 @@
+package hu.nje.beadando.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
